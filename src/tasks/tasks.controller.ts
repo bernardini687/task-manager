9c3acdffd44471389b01b012e16bddc8b61753e1
oscar bernardini <bernardini687@gmail.com>
2020-04-14 08:58:36 +0200

    setup to integrate repo in task service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,45 +10,46 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { ReadFilterDto } from './dto/read-filter-dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { TaskStatus } from './task-status.enum';
 
 @Controller('/tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Post()
-  // validates the body against the DTO.
-  create(@Body(ValidationPipe) createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.create(createTaskDto);
-  }
+  // @Post()
+  // // validates the body against the DTO.
+  // create(@Body(ValidationPipe) createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.create(createTaskDto);
+  // }
 
-  @Get()
-  read(@Query(ValidationPipe) filterDto: ReadFilterDto): Task[] {
-    if (Object.keys(filterDto).length === 0) {
-      return this.tasksService.readAll();
-    } else {
-      return this.tasksService.readFiltered(filterDto);
-    }
-  }
+  // @Get()
+  // read(@Query(ValidationPipe) filterDto: ReadFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length === 0) {
+  //     return this.tasksService.readAll();
+  //   } else {
+  //     return this.tasksService.readFiltered(filterDto);
+  //   }
+  // }
 
-  @Get('/:id')
-  readOne(@Param('id') id: string): Task {
-    return this.tasksService.readOne(id);
-  }
+  // @Get('/:id')
+  // readOne(@Param('id') id: string): Task {
+  //   return this.tasksService.readOne(id);
+  // }
 
-  @Patch('/:id/status')
-  updateStatus(
-    @Param('id') id: string,
-    @Body('status', TaskStatusValidationPipe) status: TaskStatus
-  ): Task {
-    return this.tasksService.updateStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus
+  // ): Task {
+  //   return this.tasksService.updateStatus(id, status);
+  // }
 
-  @Delete('/:id')
-  delete(@Param('id') id: string): void {
-    this.tasksService.delete(id);
-  }
+  // @Delete('/:id')
+  // delete(@Param('id') id: string): void {
+  //   this.tasksService.delete(id);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,73 +1,60 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v1 as uuid } from 'uuid';
+import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { ReadFilterDto } from './dto/read-filter-dto';
+import { TaskStatus } from './task-status.enum';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
-
-  create(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-    this.tasks.push(task);
-
-    return task;
-  }
-
-  readAll(): Task[] {
-    return this.tasks;
-  }
-
-  readFiltered(filterDto: ReadFilterDto): Task[] {
-    const { status: statusFilter, search } = filterDto;
-    let tasks = this.tasks;
-
-    if (statusFilter) {
-      tasks = tasks.filter(({ status }) => status === statusFilter);
-    }
-    if (search) {
-      tasks = tasks.filter(task =>
-        [task.title, task.description].some(field =>
-          new RegExp(search, 'i').test(field)
-        )
-      );
-    }
-
-    return tasks;
-  }
-
-  readOne(id: string): Task {
-    return this.findById(id);
-  }
-
-  updateStatus(id: string, status: TaskStatus): Task | never {
-    const taskIndex = this.tasks.findIndex(task => task.id === id);
-    if (taskIndex === -1) {
-      throw new NotFoundException();
-    }
-    this.tasks[taskIndex].status = status;
-
-    return this.tasks[taskIndex];
-  }
-
-  delete(id: string): void | never {
-    const found = this.findById(id);
-    this.tasks = this.tasks.filter(task => task.id !== found.id);
-  }
-
-  private findById(id: string): Task | never {
-    const found = this.tasks.find(task => task.id === id);
-    if (!found) {
-      throw new NotFoundException();
-    }
-
-    return found;
-  }
+  // create(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   return task;
+  // }
+  // readAll(): Task[] {
+  //   return this.tasks;
+  // }
+  // readFiltered(filterDto: ReadFilterDto): Task[] {
+  //   const { status: statusFilter, search } = filterDto;
+  //   let tasks = this.tasks;
+  //   if (statusFilter) {
+  //     tasks = tasks.filter(({ status }) => status === statusFilter);
+  //   }
+  //   if (search) {
+  //     tasks = tasks.filter(task =>
+  //       [task.title, task.description].some(field =>
+  //         new RegExp(search, 'i').test(field)
+  //       )
+  //     );
+  //   }
+  //   return tasks;
+  // }
+  // readOne(id: string): Task {
+  //   return this.findById(id);
+  // }
+  // updateStatus(id: string, status: TaskStatus): Task | never {
+  //   const taskIndex = this.tasks.findIndex(task => task.id === id);
+  //   if (taskIndex === -1) {
+  //     throw new NotFoundException();
+  //   }
+  //   this.tasks[taskIndex].status = status;
+  //   return this.tasks[taskIndex];
+  // }
+  // delete(id: string): void | never {
+  //   const found = this.findById(id);
+  //   this.tasks = this.tasks.filter(task => task.id !== found.id);
+  // }
+  // private findById(id: string): Task | never {
+  //   const found = this.tasks.find(task => task.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException();
+  //   }
+  //   return found;
+  // }
 }
